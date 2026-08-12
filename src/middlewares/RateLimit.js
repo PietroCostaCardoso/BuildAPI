ebae668b4d7a896367470323c5de88d4d9c312a4
Pietro Costa Cardoso <pietrocosta.cardoso1@gmail.com>
2026-08-12 15:57:38 -0300

    Update RateLimit.js

--- src/middlewares/RateLimit.js
+++ src/middlewares/RateLimit.js
@@ -2,12 +2,10 @@ import rateLimit from 'express-rate-limit';
 
 
 const limitReq = rateLimit({
-  // windowMs define o tamanho da janela de tempo em milissegundos (1000ms = 1 segundo)
-  windowMs: 1000, 
-  // max é o número máximo de req permitidas dentro dessa janela 
-  max: 7, 
+  windowMs: 1000, // janela de tempo em milissegundos (1000ms = 1 segundo)
+  max: 7, //número máximo de req permitidas dentro dessa janela
 
-  // Mensagem que será enviada se o usuário estourar o limite
+  //  se o usuário estourar o limite
   message: {
     status: 429,
     erro: 'Você enviou muitas requisições. espere um pouco'
